9e97539559ee2d0ae95240a9d9475f23a05e20d7
BackLitKeyboard <rasik.hasan@yahoo.com>
2020-08-04 20:03:00 +0600

    sequential calls

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,16 @@
 const first = () => {
     console.log("first");
 };
+const second = () => {
+    console.log("second");
+};
+const third = () => {
+    console.log("third");
+};
+//calling
 first();
 console.log("first is called");
+second();
+console.log("second is called");
+third();
+console.log("third is called");
